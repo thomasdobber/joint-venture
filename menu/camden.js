f39a0687244d37d33c115432757bd7ca23084ce6
Thomas Dobber <thomasdobber@gmail.com>
2025-01-15 21:55:26 +0100

    init

---
+++ menu/camden.js
@@ -0,0 +1,76 @@
+var clicked=""
+var gtype=".gif"
+var selstate="_over"
+if (typeof(loc)=="undefined" || loc==""){
+	var loc=""
+	if (document.body&&document.body.innerHTML){
+		var tt = document.body.innerHTML.toLowerCase();
+		var last = tt.indexOf("camden.js\"");
+		if (last>0){
+			var first = tt.lastIndexOf("\"", last);
+			if (first>0 && first<last) loc = document.body.innerHTML.substr(first+1,last-first-1);
+		}
+	}
+}
+
+document.write("<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\"><tr>");
+tr(false);
+writeButton(loc+"","../index.html","camden_b1",89,18,"Home","_parent",0);
+writeButton(loc+"","../agenda.html","camden_b2",89,18,"Agenda","_parent",0);
+writeButton(loc+"","../muziek.html","camden_b3",89,18,"Muziek","_parent",0);
+writeButton(loc+"","../contact.html","camden_b4",89,18,"Contact","_parent",0);
+writeButton(loc+"","../foto's.html","camden_b5",89,18,"Foto's","_parent",0);
+writeButton("","http://www.tboek.nl/gastenboek/jointventure","camden_b6",89,18,"Gastenboek","_parent",0);
+tr(true);
+document.write("</tr></table>")
+loc="";
+
+function tr(b){}
+
+function turn_over(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_over"].src;
+	}
+}
+
+function turn_off(name) {
+	if (document.images != null && clicked != name) {
+		document[name].src = document[name+"_off"].src;
+	}
+}
+
+function reg(gname,name)
+{
+if (document.images)
+	{
+	document[name+"_off"] = new Image();
+	document[name+"_off"].src = loc+gname+gtype;
+	document[name+"_over"] = new Image();
+	document[name+"_over"].src = loc+gname+"_over"+gtype;
+	}
+}
+
+function evs(name){ return " onmouseover=\"turn_over('"+ name + "')\" onmouseout=\"turn_off('"+ name + "')\""}
+
+function writeButton(urld, url, name, w, h, alt, target, hsp)
+{
+	gname = name;
+	while(typeof(document[name])!="undefined") name += "x";
+	reg(gname, name);
+	tr(true);
+	document.write("<td>");
+	if (alt != "") alt = " alt=\"" + alt + "\"";
+	if (target != "") target = " target=\"" + target + "\"";
+	if (w > 0) w = " width=\""+w+"\""; else w = "";
+	if (h > 0) h = " height=\""+h+"\""; else h = "";	
+	if (url != "") url = " href=\"" + urld + url + "\"";
+	
+	document.write("<a " + url + evs(name) + target + ">");	
+	
+	if (hsp == -1) hsp =" align=\"right\"";
+	else if (hsp > 0) hsp = " hspace=\""+hsp+"\"";
+	else hsp = "";
+	
+	document.write("<img src=\""+loc+gname+gtype+"\" name=\"" + name + "\"" + w + h + alt + hsp + " border=\"0\" /></a></td>");
+	tr(false);
+}
